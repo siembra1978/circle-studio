f37f5710168d4f366d228f4faefa7ae79be21112
siembra <77788776+siembra1978@users.noreply.github.com>
2024-09-18 14:48:28 -0400

    t

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 import pygame
+import struct
 import replaydecoder
+
+printInfo = False
 
 selectedReplay = 'std.osr'
 
 replayInfo = replaydecoder.readReplay(selectedReplay)
 
-for detail in replayInfo:
-    if detail != "replay":
-        print(detail, ":", replayInfo.get(detail))
+if printInfo:
+    for detail in replayInfo:
+        if detail != "replay":
+            print(detail, ":", replayInfo.get(detail))
 
-print(replayInfo.get("replay"))
+#print(replayInfo.get("replay"))
+
+replayData = replayInfo.get("replay")
+byteList = []
+countDict = {}
+
+
+for byte in replayData:
+
+    byteList.append(byte)
+
+    if str(byte) in countDict.keys():
+        countDict[str(byte)] = int(countDict.get(str(byte))) + 1
+    elif str(byte) not in countDict:
+        countDict[str(byte)] = 1
+
+compareList = []
+
+for byte in byteList:
+    print(byte)
